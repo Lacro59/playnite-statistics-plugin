d8d2643e1c53ce2919bd414d53807de41f987c2b
Lacro59 <lacro59@gmail.com>
2020-06-02 17:43:42 +0200

    Fix version.

--- Statistics.cs
+++ Statistics.cs
@@ -6,11 +6,7 @@ using Statistics.Views;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Controls;
 
 namespace Statistics
 {
